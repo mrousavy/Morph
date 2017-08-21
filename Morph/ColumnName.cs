97c1a045da33aedde0b7cee97f847b58d17c14d2
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 22:08:15 +0200

    Add ColumnName Attribute

---
+++ Morph/ColumnName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mrousavy.Morph {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// <see cref="ColumnName"/> <see cref="Attribute"/> for parsing from a Database to a .NET Object
+    /// </summary>
+    public class ColumnName : Attribute {
+        public string Name { get; }
+
+        public ColumnName(string name) {
+            Name = name;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Morph
 <p align="center">
   <img src="https://raw.githubusercontent.com/mrousavy/Morph/master/Images/emoji_hammer_and_wrench.png" height="120" />
   <h3 align="center">Morph</h3>
