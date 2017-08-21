134079eafe935a8ce28bcf1b7b5c4b358f52f51a
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 20:16:39 +0200

    Add .NET Core unit tests

---
+++ Morph.Tests/MorphTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Morph.Tests {
+    [TestClass]
+    public class MorphTests {
+        [TestMethod]
+        public void TestMethod1() {
+            
+        }
+    }
+}
