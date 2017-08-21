568a51648f2f31dce8b1ad3c001288b80ea71c9d
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 22:08:37 +0200

    Add basic unit test

--- Morph.Tests/MorphTests.cs
+++ Morph.Tests/MorphTests.cs
@@ -1,11 +1,54 @@
+using System.Data;
+using System.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mrousavy.Morph;
 
 namespace Morph.Tests {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// Unit Test Class for testing Morph functions
+    /// </summary>
     [TestClass]
     public class MorphTests {
+        private SqlConnection Connection { get; set; }
+        private IDataReader Reader { get; set; }
+
+        private const string SqlServer = "";
+        private const string SqlDatabase = "";
+        private const string SqlUser = "";
+        private const string SqlPass = "";
+
+        [TestInitialize]
+        public void Initialize() {
+            Connection = new SqlConnection($"Server={SqlServer};Database={SqlDatabase};User ID={SqlUser};Password={SqlPass}"); //MSSQL
+            //Fire command
+            using (IDbCommand command = new SqlCommand("SELECT * FROM Person;", Connection)) {
+                Reader = command.ExecuteReader();
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup() {
+            Connection?.Dispose();
+            Reader?.Dispose();
+        }
+
         [TestMethod]
-        public void TestMethod1() {
-            
+        public void FullTest() {
+            //Fire command
+            using (IDbCommand command = new SqlCommand("SELECT * FROM Person;", Connection)) {
+                //execute reader
+                using (IDataReader reader = command.ExecuteReader()) {
+                    Person person = reader.Parse<Person>(); //parse to person
+                    Assert.IsNotNull(person);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ParseTest() {
+            Person person = Reader.Parse<Person>(); //parse to person
+            Assert.IsNotNull(person);
         }
     }
 }
---
+++ Morph.Tests/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using mrousavy.Morph;
+
+namespace Morph.Tests {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// Test Person object representing a Person in the Database
+    /// </summary>
+    public class Person {
+        /// <summary>
+        /// This <see cref="Person"/>'s first name
+        /// </summary>
+        [ColumnName("FIRST_NAME")]
+        public string FirstName { get; set; }
+        /// <summary>
+        /// This <see cref="Person"/>'s last name
+        /// </summary>
+        [ColumnName("LAST_NAME")]
+        public string LastName { get; set; }
+        /// <summary>
+        /// This <see cref="Person"/>'s address
+        /// </summary>
+        [ColumnName("ADDRESS")]
+        public string Address { get; set; }
+    }
+}
