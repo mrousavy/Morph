952ec05e30040a140cf30959328c17bfd9deda4b
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 22:08:23 +0200

    Add Parser concepts

--- Morph/Parser.cs
+++ Morph/Parser.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Data;
 using System.Data.Common;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace mrousavy.Morph {
-    internal class Parser {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// Internal static class for parsing Values from 
+    /// a Data Reader (<see cref="IDataReader"/>, <see cref="DbDataReader"/>)
+    /// to managed .NET objects via reflecting on <see cref="ColumnName"/> attributes.
+    /// </summary>
+    public static class Parser {
 
-        internal static async Task<T> ParseAsync<T>(DbDataReader reader) {
+        public static async Task<T> ParseAsync<T>(DbDataReader reader) {
             T result = default(T);
+            Type type = typeof(T);
+
+            //read all returned rows
             while (await reader.ReadAsync()) {
                 //fill attributes
-                Type type = typeof(T);
+                var properties = type.GetRuntimeProperties();
+                var attributes = properties.Select(property => property.Attributes);
+                try {
+                    
+                } catch {
+                    //value could not be parsed
+                }
             }
             return result;
         }
 
-        internal static T Parse<T>(IDataReader reader) {
+        public static T Parse<T>(IDataReader reader) {
             T result = default(T);
             while (reader.Read()) {
                 //fill attributes
