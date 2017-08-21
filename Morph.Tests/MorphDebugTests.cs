4cfbd0c464485067ce98b4bc3864d96453212618
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 22:08:58 +0200

    Add tests for debug

---
+++ Morph.Tests/MorphDebugTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using mrousavy.Morph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Morph.Tests {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// Unit Test Class for testing Morph functions (DEBU)G
+    /// </summary>
+    [TestClass]
+    public class MorphDebugTests {
+
+        [TestMethod]
+        public void FullTest() {
+            var person = Parser.ParseAsync<Person>(null);
+        }
+    }
+}
