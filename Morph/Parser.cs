c7286c1bd49728388bb5ce2218d12f89ccb79442
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 20:16:28 +0200

    Add parser class

---
+++ Morph/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace mrousavy.Morph {
+    internal class Parser {
+
+        internal static async Task<T> ParseAsync<T>(DbDataReader reader) {
+            T result = default(T);
+            while (await reader.ReadAsync()) {
+                //fill attributes
+                Type type = typeof(T);
+            }
+            return result;
+        }
+
+        internal static T Parse<T>(IDataReader reader) {
+            T result = default(T);
+            while (reader.Read()) {
+                //fill attributes
+                Type type = typeof(T);
+            }
+            return result;
+        }
+    }
+}
