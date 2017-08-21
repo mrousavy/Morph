cbbdf6dbdca9ac272e6c3601f8577018d39d5ee9
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 20:16:17 +0200

    Add base IDataReader extension class

---
+++ Morph/DbReaderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace mrousavy.Morph {
+    public static class DbReaderExtensions {
+
+        public static async Task<T> Parse<T>(this IDataReader @this) {
+            if(@this == null) throw new ArgumentNullException(nameof(@this)); //check parameter
+
+            //if no DbDataReader object -> throw
+            if (!(@this is DbDataReader reader)) throw new ArgumentException(nameof(@this));
+
+            return await Parser.ParseAsync<T>(reader);
+        }
+    }
+}
