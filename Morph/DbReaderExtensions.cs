706f59064b7d781217cc7542ae46a6ec32da13b9
mrousavy <mrousavy@donotreply.github.com>
2017-08-21 22:08:51 +0200

    Implement and document Extensions

--- Morph/DbReaderExtensions.cs
+++ Morph/DbReaderExtensions.cs
@@ -4,15 +4,39 @@ using System.Data.Common;
 using System.Threading.Tasks;
 
 namespace mrousavy.Morph {
+    /// <author>Marc Rousavy</author>
+    /// <summary>
+    /// Extension class for providing Morph Parse functions for
+    /// <see cref="IDataReader"/> and <see cref="DbDataReader"/>
+    /// </summary>
     public static class DbReaderExtensions {
 
-        public static async Task<T> Parse<T>(this IDataReader @this) {
+        /// <summary>
+        /// Asynchronously parses values from this <see cref="IDataReader"/>
+        /// to a managed .NET object <see cref="T"/> where Properties of <see cref="T"/> must be public and flagged
+        /// with the <see cref="ColumnName"/> attribute.
+        /// </summary>
+        public static async Task<T> ParseAsync<T>(this IDataReader @this) {
             if(@this == null) throw new ArgumentNullException(nameof(@this)); //check parameter
+            
+            //if no DbDataReader object -> throw
+            if (!(@this is DbDataReader reader)) throw new ArgumentException(nameof(@this));
+            
+            return await Parser.ParseAsync<T>(reader); //actually parse
+        }
+
+        /// <summary>
+        /// Parses values from this <see cref="IDataReader"/>
+        /// to a managed .NET object <see cref="T"/> where Properties of <see cref="T"/> must be public and flagged
+        /// with the <see cref="ColumnName"/> attribute.
+        /// </summary>
+        public static T Parse<T>(this IDataReader @this) {
+            if (@this == null) throw new ArgumentNullException(nameof(@this)); //check parameter
 
             //if no DbDataReader object -> throw
             if (!(@this is DbDataReader reader)) throw new ArgumentException(nameof(@this));
 
-            return await Parser.ParseAsync<T>(reader);
+            return Parser.Parse<T>(reader); //actually parse
         }
     }
 }
